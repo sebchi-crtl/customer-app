ff40922743db9b4d7e3ca7bfa8f5eaf28b5a2ac7
Nwobodo Chiemelie Sebastine <chiemelienwobodo8@gmail.com>
2021-12-29 21:53:51 +0100

    after

--- src/components/AddUserComponent.js
+++ src/components/AddUserComponent.js
@@ -22,12 +22,34 @@ class AddUserComponent extends Component {
             return
         }
         else{
-            this.componentDidMount
+            axios.post('http://localhost:8080/api/v1/customer', 
+            {   
+                name,
+                email,
+                gender
+            })
+            .then(res=>{
+                console.log(res);
+                console.log(res.data); //This line of code will redirect you once the submission is succeed
+            })
+            .catch(error => {
+                console.error('There is an error!', error);
+                console.log(' error!', error);
+                console.log('There is an error!', error.message);
+                console.log('There is an error!', error.data);
+                console.log('There is an error!', error.request);
+                console.log('There is an error!', error.response.headers);
+                console.log('There is an error!', error.response.status);
+                console.log('There is an error!', error.response.statusText);
+                console.log('There is an error!', error.response.config);
+                console.log('There is an error!', error.request.method);
+                console.log('There is an error!', error.response.data);
+                console.log('There is an error!', error.toJSON());
+            });
         }
       
 
     }
-
 
     render() {
         return (
